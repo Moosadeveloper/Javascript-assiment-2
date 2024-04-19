7b09f314ead20c129c6a1f7986b6de2a0beb4a85
Moosadeveloper <janmoosa77@gmail.com>
2024-04-19 22:02:17 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var ZipCode = prompt("Your city zip code");
+if (ZipCode === "10010") {
+    alert("karachi");
+} else {
+    alert("Please write correct city");
+}
+
+var ZipCode = prompt("Another city zip code");
+if (ZipCode === "51000") {
+    alert("lahore");
+} else {
+    alert("Please write correct city");
+}
+
+
+var ZipCode = prompt("Another city zip code");
+if (ZipCode === "54700") {
+    alert("punjab");
+} else {
+    alert("Please write correct city");
+}
+
+var ZipCode = prompt("Another city zip code");
+if (ZipCode === "87300") {
+    alert("quetta");
+} else {
+    alert("Please write correct city");
+}
+
+
+var ZipCode = prompt("Another city zip code");
+if (ZipCode === "44080") {
+    alert("islambad");
+} else {
+    alert("Please write correct city");
+}
+
